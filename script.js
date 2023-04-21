f229f5080499549842ef824ff456cd1eddd1814e
morphius <inneskgibson@gmail.com>
2023-04-21 01:43:11 +0100

    mark 6 search funtion

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ form.addEventListener('submit', (e) => {
   // Fetch the mock data
   const mockData = [
     {
-      title: 'Recipe 1',
-      ingredients: ['apple', 'ingredient 2', 'ingredient 3']
+      name: "Spaghetti Bolognese",
+      ingredients: ['spaghetti', "ground beef", "tomato sauce", "onions", "garlic", "basil"],
+      instructions: "1. Cook spaghetti according to package instructions.\n 2. In a separate pan, cook ground beef until browned.\n 3. Add chopped onions and garlic to the pan and cook until soft.\n 4. Stir in tomato sauce and basil.\n 5. Serve the sauce over cooked spaghetti.",
     },
     {
       title: 'Recipe 2',
