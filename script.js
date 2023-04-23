e185ea5306926a9cda96ff32aa17db746a039929
morphius <inneskgibson@gmail.com>
2023-04-23 09:03:11 +0100

    mark 6
    added search bar, worked css files for layout

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const form = document.querySelector('form');
-const input = document.querySelector('input');
+const searchInput = document.querySelector('#search-input');
+const API_KEY = '1';
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const query = input.value;
+if (form) {
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const searchTerm = searchInput.value;
 
-  // Fetch the mock data
-  const mockData = [
-    {
-      name: "Spaghetti Bolognese",
-      ingredients: ['spaghetti', "ground beef", "tomato sauce", "onions", "garlic", "basil"],
-      instructions: "1. Cook spaghetti according to package instructions.\n 2. In a separate pan, cook ground beef until browned.\n 3. Add chopped onions and garlic to the pan and cook until soft.\n 4. Stir in tomato sauce and basil.\n 5. Serve the sauce over cooked spaghetti.",
-    },
-    {
-      title: 'Recipe 2',
-      ingredients: ['ingredient 2', 'ingredient 4', 'ingredient 5']
-    },
-    {
-      title: 'Recipe 3',
-      ingredients: ['ingredient 3', 'ingredient 5', 'ingredient 6']
-    }
-  ];
-
-  // Filter the data based on the search query
-  const results = mockData.filter(recipe => recipe.ingredients.includes(query));
-
-  // Redirect to the results page and pass the search results as a query string
-  window.location.href = `results.html?results=${encodeURIComponent(JSON.stringify(results))}`;
-});
+    fetch(`https://www.themealdb.com/api/json/v1/${API_KEY}/search.php?s=${searchTerm}`, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.meals && data.meals.length > 0) {
+          const queryString = '?results=' + encodeURIComponent(JSON.stringify(data.meals));
+          window.location.href = 'results.html' + queryString;
+        } else {
+          window.location.href = 'results.html?results=';
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
+}
